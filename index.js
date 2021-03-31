ca0a79b3b80f1a199ce2e3256f7c7f6ba6fad1be
Jesse Günzl <20597157+Emtrixx@users.noreply.github.com>
2021-03-31 15:15:43 +0200

    add navbar and boilerplate layout with ejs-mate

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
+const ejsmate = require("ejs-mate");
 
+app.engine('ejs', ejsmate);
 app.set("view engine", "ejs");
+app.set("views", (__dirname+"/views"))
+
+app.use(express.static(__dirname+"/public"));
 
 app.get('/', (req,res) => {
     res.render('index');
